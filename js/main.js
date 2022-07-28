c7853d34cb2034d6afb34200473a330f155fc7f9
Gabriel4g <gabrielprogramador11@gmail.com>
2022-07-28 14:00:19 -0500

    ignore

--- js/main.js
+++ js/main.js
@@ -16,34 +16,14 @@ function addTask() {
     } 
 
     let liTasks = document.createElement("li");
-
+    
     array.unshift(liTasks);
 
     liTasks.innerHTML = input_tasks.value;
 
     tasks.append(liTasks);
 
-    liTasks.className = "tasksStyle";
-    let classStyle = liTasks.classList;
-
-    classStyle.add("tasksStyle");
-    
-    /* const stylesTask = {
-        background: liTasks.style.background = "#2a2a2a",
-        color: liTasks.style.color = "#fff",
-        width: liTasks.style.width = "auto",
-        maxWidth: liTasks.style.maxWidth = "16rem",
-        overflow: liTasks.style.overflow = "hidden",
-        radius: liTasks.style.borderRadius = "5px",
-        padding: liTasks.style.padding = "10px 10px",
-        margin: liTasks.style.margin = "10px",
-    }
-
-    const stylesInput = {
-        weight: liTasks.style.fontWeight = "500",
-        family: liTasks.style.fontFamily = "Roboto",
-        listStyle: liTasks.style.listStyle = "none"
-    } */
+    liTasks.classList.add("tasksStyle");
 
     input_tasks.value = ""
 
